169af3140d5ad10f507860097660a72d5c62782c
nrths <northnosouth@gmail.com>
2021-09-24 06:28:22 +0300

    added js

--- scripts/index.js
+++ scripts/index.js
@@ -61,7 +61,7 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
     ];
-
+// карточка для проверки добавления: Мурманск, https://images.unsplash.com/photo-1615924631431-3a5c33d767d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80
     
 
 // функция создания новой карточки
