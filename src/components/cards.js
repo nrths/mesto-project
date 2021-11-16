80a6a098e41d098f7d983394ec7a01800f3a6087
nrths <northnosouth@gmail.com>
2021-11-16 18:31:06 +0300

    fixes after review (1st level)

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,6 +1,5 @@
 import { openPopupFunc } from "./modal.js";
 
-const photoGrid = document.querySelector('.elements');
 const cardShowPopup = document.querySelector('.popup__mode_card-show');
 const popupImage = cardShowPopup.querySelector('.popup__figure-image');
 const popupCaption = cardShowPopup.querySelector('.popup__figcaption');
@@ -44,44 +43,4 @@ function makeNewCard(name, link) {
   return newCard;
 }
 
-// функция добавления карточки в панель (spread - use array elements in agruments, dont forget)
-function addNewCard(photoGrid, ...initialCards) {
-  initialCards.forEach((card) => {
-    photoGrid.prepend(makeNewCard(card.name, card.link));
-  });
-}
-
-// базовые карточки
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-    ];
-// карточка для проверки добавления: Мурманск, 
-// https://images.unsplash.com/photo-1615924631431-3a5c33d767d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80
-    
-// инициализация базовых карточек
-addNewCard(photoGrid, ...initialCards);
-
-export { photoGrid, makeNewCard, addNewCard }
+export { makeNewCard }
